d4558d0001c810e5d00d518c599746e00ba4929d
Rajko Vrga <rajkovrga@yahoo.com>
2018-10-23 21:17:24 +0200

    first commit

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace EnglishTranslate
+{
+    public partial class Form1 : Form
+    {
+
+        private static int Global1;
+        private static int Global2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string word = textBox1.Text;
+            string[] file;
+            try
+            {
+                file = File.ReadAllLines(@"../../translate.txt");
+            }
+            catch (FileNotFoundException er)
+            {
+                MessageBox.Show(er.ToString());
+                return;
+            }
+            catch(IOException er)
+            {
+                MessageBox.Show(er.ToString());
+                return;
+
+            }
+            catch (UnauthorizedAccessException er)
+            {
+                MessageBox.Show(er.ToString());
+                return;
+
+            }
+
+            Translate tr = new Translate(word);
+            foreach (string line in file)
+                {
+
+                    Global1 = tr.TranslaterEng(line.Split('-')[0]);
+                    Global2 = tr.TranslaterSrb(line.Split('-')[1]);
+                    if (Global1 == 1)
+                    {
+                        textBox2.Text = line.Split('-')[1];
+                    }
+                    else if (Global2 == 1)
+                    {
+                        textBox2.Text = line.Split('-')[0];
+                    }
+                    else
+                    {
+                        textBox2.Text = "Ne postoji uneta rec";
+                    }
+
+
+                }
+          
+           
+           
+           
+        }
+
+       
+    }
+}
---
+++ Translate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTranslate
+{
+    class Translate
+    {
+
+        private string word;
+
+        public Translate(string word)
+        {
+            this.word = word;
+        }
+
+        public string Word
+        {
+            get
+            { return word; }
+            set
+            {
+                word = value;
+            }
+        }
+
+        public int TranslaterSrb(string fWordSrb)
+        {
+            if (fWordSrb == this.word)
+            {
+                return 1;
+
+            }
+            else
+            {
+                return 0;
+            }
+            
+
+          
+        }
+        public int TranslaterEng(string fWordEng)
+        {
+            if (fWordEng == this.word)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+
+
+        }
+
+
+
+    }
+}
